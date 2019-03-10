be4e83f371ca5584230bcdd03e6a2ec8fb8d84ea
Sviatoslav Filippov <filippov.sviatoslav@gmail.com>
2019-03-10 18:41:40 +0200

    Remove symbols kata (extended)

---
+++ Codewars/RemoveSymbols.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace Katas
+{
+    public class RemoveSymbols
+    {
+        public static string Remove(string input, int count, char symbol)
+        {
+            var outputSb = new StringBuilder(input.Length);
+            var removed = 0;
+
+            foreach (var currentSymbol in input)
+            {
+                if (currentSymbol == symbol && removed < count)
+                {
+                    removed++;
+                    continue;
+                }
+
+                outputSb.Append(currentSymbol);
+            }
+
+            return outputSb.ToString();
+        }
+    }
+}
---
+++ Tests/RemoveSymbolsTests.cs
@@ -0,0 +1,25 @@
+﻿using Katas;
+using Xunit;
+
+namespace Tests
+{
+    public class RemoveSymbolsTests
+    {
+        [Theory]
+        [InlineData("Hi!", 1, '!', "Hi")]
+        [InlineData("Hi!", 100, '!', "Hi")]
+        [InlineData("Hi!!!", 1, '!', "Hi!!")]
+        [InlineData("Hi!!!", 100, '!', "Hi")]
+        [InlineData("!Hi", 1, '!', "Hi")]
+        [InlineData("!Hi!", 1, '!', "Hi!")]
+        [InlineData("!Hi!", 100, '!', "Hi")]
+        [InlineData("!!!Hi !!hi!!! !hi", 1, '!', "!!Hi !!hi!!! !hi")]
+        [InlineData("!!!Hi !!hi!!! !hi", 3, '!', "Hi !!hi!!! !hi")]
+        [InlineData("!!!Hi !!hi!!! !hi", 5, '!', "Hi hi!!! !hi")]
+        [InlineData("!!!Hi !!hi!!! !hi", 100, '!', "Hi hi hi")]
+        public void Should(string input, int count, char symbol, string output)
+        {
+            Assert.Equal(output, RemoveSymbols.Remove(input, count, symbol));
+        }
+    }
+}
