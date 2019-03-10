14c05477277ada4688bfb327a989a5615ab148ec
Sviatoslav Filippov <filippov.sviatoslav@gmail.com>
2019-03-10 19:21:20 +0200

    Middle character kata

---
+++ Codewars/MiddleCharacter.cs
@@ -0,0 +1,13 @@
+﻿namespace Katas
+{
+    public class MiddleCharacter
+    {
+        public static string GetMiddle(string input)
+        {
+            var middleIndex = input.Length / 2;
+            return input.Length % 2 == 0
+                ? $"{input[middleIndex - 1]}{input[middleIndex]}"
+                : $"{input[middleIndex]}";
+        }
+    }
+}
---
+++ Tests/MiddleCharacterTests.cs
@@ -0,0 +1,16 @@
+﻿using Katas;
+using Xunit;
+
+namespace Tests
+{
+    public class MiddleCharacterTests
+    {
+        [Theory]
+        [InlineData("1234", "23")]
+        [InlineData("ABC", "B")]
+        public void Should(string input, string output)
+        {
+            Assert.Equal(output, MiddleCharacter.GetMiddle(input));
+        }
+    }
+}
