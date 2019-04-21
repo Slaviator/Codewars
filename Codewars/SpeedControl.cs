ff1355151aaaf7aab9fb2303bd222a69ac750505
Sviatoslav Filippov <filippov.sviatoslav@gmail.com>
2019-04-21 22:43:02 +0300

    SpeedControl c#

---
+++ Codewars/SpeedControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas.CSharp
+{
+    public class SpeedControl
+    {
+        public static int Gps(int period, IEnumerable<double> distances)
+            => distances.Aggregate(new {value = .0, speed = 1},
+                (speedAndValue, nextValue) => new
+                {
+                    value = nextValue,
+                    speed = Math.Max((int) (3600 * (nextValue - speedAndValue.value) / period), speedAndValue.speed)
+                }, speedAndValue => speedAndValue.speed);
+    }
+}
